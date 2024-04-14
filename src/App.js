53ecff7c38d1de97dae205431d513c596d6ea135
shankey raheja <shankeyraheja@gmail.com>
2024-04-14 19:46:02 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import React, { useEffect } from 'react';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+import {Button} from "@mui/material"
+
+
 
 function App() {
+  
+  // dayjs.tz.setDefault('Pacific/Auckland');
+  let date = new Date()
+  const [value, setValue] = React.useState(dayjs(date));
+  
+
+  
+  const handleClick = () => {
+    alert(`Your Appointment is booked on ${dayjs(value).format('DD-MM-YYYY')}`)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker           
+      value={value}
+      onChange={(newValue) => {
+        // console.log(newValue)
+        setValue(newValue)
+        }}></DatePicker>
+        <Button variant="contained" onClick={handleClick} style={{marginLeft:"70%"}}>BOOK APPOINTMENT</Button>
+      </LocalizationProvider>
     </div>
   );
 }
